7a46cc5c146363424446871aa3cf4c9fd436317c
curro casado <currocuo9625@gmail.com>
2020-01-24 19:40:56 -0600

    Display child component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
 
+let bookList = [
+  { title: "The Lord of the Rings", author: "J. R. R. Tolkien", pages: 1595 },
+  {
+    title: "Harry Potter and the Philosopher stone",
+    author: "J. K. Rowling",
+    pages: 300
+  },
+  {
+    title: "Harry Potter and Prisioner of Azkaban",
+    author: "J. K. Rowling",
+    pages: 294
+  },
+  {
+    title: "Harry Potter and the secret chamber",
+    author: "J. K. Rowling",
+    pages: 460
+  }
+];
+
 const Book = ({ title, author, pages }) => {
   return (
     <section>
@@ -11,26 +30,19 @@ const Book = ({ title, author, pages }) => {
   );
 };
 
-const Library = () => {
+const Library = ({ books }) => {
   return (
     <div>
-      <Book
-        title="The Lord of the Rings"
-        author="J. R. R. Tolkien"
-        pages={1595}
-      />
-      <Book
-        title="Harry Potter and the Philosopher stone"
-        author="J. K. Rowling"
-        pages={300}
-      />
-      <Book
-        title="Harry Potter and Prisioner of Azkaban"
-        author="J. K. Rowling"
-        pages={293}
-      />
+      {books.map((book, i) => (
+        <Book
+          key={i}
+          title={book.title}
+          author={book.author}
+          pages={book.pages}
+        />
+      ))}
     </div>
   );
 };
 
-render(<Library />, document.getElementById("root"));
+render(<Library books={bookList} />, document.getElementById("root"));
