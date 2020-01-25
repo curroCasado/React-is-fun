82837b5fb6e988f4d9b930d11efeba9add169e55
curro casado <currocuo9625@gmail.com>
2020-01-25 14:08:18 -0600

    Fetchiing data and component life cycle

--- src/index.js
+++ src/index.js
@@ -43,7 +43,25 @@ const NotHiring = () => (
 );
 
 class Library extends Component {
-  state = { open: true, freeBookmark: true, hiring: false };
+  state = {
+    open: true,
+    freeBookmark: true,
+    hiring: false,
+    data: [],
+    loading: false
+  };
+
+  componentDidMount() {
+    this.setState({ loading: true });
+    fetch("http://hplussport.com/api/products/order/price/sort/asc/qty/1")
+      .then(data => data.json())
+      .then(data => this.setState({ data, loading: false }));
+
+    //console.log("The component is now mounted!");
+  }
+  componentDidUpdate() {
+    console.log("The component just updated!");
+  }
 
   toggleOpenClosed = () => {
     this.setState(prevState => ({
@@ -55,6 +73,21 @@ class Library extends Component {
     return (
       <div>
         {this.state.hiring ? <Hiring /> : <NotHiring />}
+        {this.state.loading ? (
+          "loading..."
+        ) : (
+          <div>
+            {this.state.data.map(product => {
+              return (
+                <div>
+                  <h3>Library Product of the Week!</h3>
+                  <h4>{product.name}</h4>
+                  <img src={product.image} height={100} />
+                </div>
+              );
+            })}
+          </div>
+        )}
         <h1>The library is {this.state.open ? "open" : "closed"}</h1>
         <button onClick={this.toggleOpenClosed}>
           {this.state.open ? "close" : "open"}
